2ba042dc2aa81651e9465cc42ae95d2a3488ce3b
charles <charlestheurkauf@outlook.com>
2022-11-10 20:45:46 -0500

    cleaned up code, added a couple comments

--- Python/getdata.py
+++ Python/getdata.py
@@ -1,20 +1,21 @@
-
 import serial
 import time
 
+
 def parse_data(data):
+    """Cleans up the byte string into and integer"""
     return int(str(data)[2:].split("\\")[0])
 
-arduino=serial.Serial(port="/dev/ttyACM0")
-time.sleep(1)
-data=[]
-times=[]
-start=time.time()
-while True:
-    new_data=parse_data(arduino.readline())
-    now=time.time()-start
-    data.append(new_data)
-    times.append(now)
 
+# Open port
+arduino = serial.Serial(port="/dev/ttyACM0")
+# Sleep to let the arduino start
+time.sleep(1)
+start = time.time()
+
+while True:
+    # Read a line of data from arduino
+    new_data = parse_data(arduino.readline())
+    now = time.time() - start
 
     print(f"time:{now},data:{new_data}")
--- Python/helloworld.py
+++ Python/helloworld.py
@@ -1,6 +1,9 @@
 import serial
 import time
-arduino=serial.Serial(port="/dev/ttyACM0")
+
+# Very simple example of how to use py serial with arduino
+# Note: The port may be different on different computers!! check if arduino IDE
+arduino = serial.Serial(port="/dev/ttyACM0",baudrate=9600)
 time.sleep(1)
-results=arduino.readline()
+results = arduino.readline()
 print(results)
